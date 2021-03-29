a061ae53c87cc3aa179b73959a6016fe5b7abf42
Igor Serbin <serbin.igor@gmail.com>
2021-03-29 22:20:11 +0300

    spotless test
    comment: add https://github.com/diffplug/spotless/tree/main/plugin-gradle#groovy
    
    @resolve: no data here

--- app/src/main/java/me/iserbin/template/MainActivity.kt
+++ app/src/main/java/me/iserbin/template/MainActivity.kt
@@ -3,12 +3,7 @@ package me.iserbin.template
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity :
-    AppCompatActivity(
-
-
-
-    ) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
